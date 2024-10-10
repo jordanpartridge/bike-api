3c3863490d5a0382dc7feebf635c8f69353d39b8
Jordan <jordan.l.partridge@gmail.com>
2024-10-09 21:22:48 -0700

    fix test

--- tests/Feature/Http/RedirectTest.php
+++ tests/Feature/Http/RedirectTest.php
@@ -1,8 +1,10 @@
 <?php
 
-it('sends to filament', function () {
-    $response = $this->get('/');
+use function Pest\Laravel\get;
+
+it('redirects unauthenticated users to login page', function () {
+    $response = get('/admin');
 
     $response->assertStatus(302);
-    $response->assertRedirect('/admin');
+    $response->assertRedirect('/admin/login');
 });
