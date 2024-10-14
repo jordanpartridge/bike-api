c14eb078d5b491f427b8cdafed00ed7362aea86d
Jordan <jordan.l.partridge@gmail.com>
2024-10-13 18:45:26 -0700

    PR adjustments

--- tests/Feature/Ride/RideModelTest.php
+++ tests/Feature/Ride/RideModelTest.php
@@ -4,5 +4,7 @@ use App\Models\Ride;
 
 it('has a working factory', function () {
     $ride = Ride::factory()->create();
-    expect($ride)->toBeInstanceOf(Ride::class);
+    expect($ride)->toBeInstanceOf(Ride::class)
+        ->and($ride->external_id)->toBeString()
+        ->and($ride->recorded_at)->toBeInstanceOf(DateTime::class);
 });
